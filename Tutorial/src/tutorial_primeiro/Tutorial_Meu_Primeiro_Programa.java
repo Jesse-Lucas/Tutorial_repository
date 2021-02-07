efb9b30493cd2982590c9094b602afd6a9f58f97
Jesse-Lucas <42957077+Jesse-Lucas@users.noreply.github.com>
2021-02-07 08:20:30 -0300

    Tutorial do meu primeiro commit

---
+++ Tutorial/src/tutorial_primeiro/Tutorial_Meu_Primeiro_Programa.java
@@ -0,0 +1,11 @@
+package tutorial_primeiro;
+
+public class Tutorial_Meu_Primeiro_Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Tutorial do meu primeiro programa em java!");
+
+	}
+
+}
